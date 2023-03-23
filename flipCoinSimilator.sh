6d046e6e8959e19ab9628159a3e3f73aeb4beefb
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-03-23 18:50:55 +0530

    Add [Nitesh] Multiple Times filppinga coin

--- flipCoinSimilator.sh
+++ flipCoinSimilator.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -x
-randomNumber=$(($RANDOM%2))
-if [ $randomNumber == 0 ]
-then 
- 	echo "Heads"
-else
-	echo "Tails"
-fi
+i=0
+Head=0
+Tail=0
+while [ $i -lt 50 ]
+do
+        randomNumber=$(($RANDOM%2))
+	if [ $randomNumber == 0 ]
+	then 
+ 	    let Head=$Head+1
+	else
+	    let Tail=$Tail+1
+	fi
+	let i++
+done
+echo " Head = $Head  Tail = $Tail "
